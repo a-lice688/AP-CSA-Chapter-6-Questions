d2856af3cc8096d619066d3ea097543ac272c3d8
a-lice688 <152440350+a-lice688@users.noreply.github.com>
2025-10-15 13:35:10 -0700

    Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Palindrome
-{
+public class Palindrome {
+    
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
